4dcac60eb81286191a8e5118a51dea28c67f9e18
Hiroki Abe <arith.inawashiro@gmail.com>
2022-01-03 15:20:08 +0900

    山一覧をマップ表示できるようにした

--- README.md
+++ README.md
@@ -1,46 +1,19 @@
-# Getting Started with Create React App
+# 日本の主な山岳一覧 フロントエンド
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## プロジェクトの概要
 
-## Available Scripts
 
-In the project directory, you can run:
+山梨県オープンデータAPIプロジェクトで公開しているAPI
+日本の主な山岳一覧 (国土地理院データ) API (https://github.com/opendata-yamanashi/yama-api)
+を使用して作成したWebアプリケーションです.
 
-### `yarn start`
+このプロジェクトは [Create React App](https://github.com/facebook/create-react-app)
+をベースに作成されています.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## 起動方法
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
 
-### `yarn test`
+1. 日本の主な山岳一覧 (国土地理院データ) API (https://github.com/opendata-yamanashi/yama-api)を8080ポートで起動後(`uvicorn app.main:app --port 8080`)します.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+2. `yarn start` で開始します.
 
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import L, { LatLng } from "leaflet"
+import "leaflet/dist/leaflet.css"
+import axios, {AxiosResponse} from "axios"
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function App() {
+  const url = "http://localhost:8080"
+  const position = new LatLng(35.667160, 138.568993);
+
+  const [yamalist, setYamaList] = useState<Array<{[key: string]: string}>>([])
+
+  // 山梨県が含まれる山一覧を取得する
+  const getYamaFromBackend = async () => {
+    await axios.create({baseURL: url}).get("?keys=都道府県&values=山梨県", 
+    {
+      headers : { Accept: "application/json"}
+    }).then(function (response: AxiosResponse) {
+      let values : Array<{[key: string]: string}> = response.data.data
+      setYamaList(values)      
+    })
+    .catch(function (error: AxiosResponse) {
+      console.log(error)
+    })    
+  }
+
+  useEffect(() => {
+    // 山梨県が含まれる山一覧を取得する
+    void getYamaFromBackend()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={position} zoom={13}>
+      <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {
+          yamalist.map((yama)=>{
+            if (typeof yama !== "undefined" && "url" in yama){
+              let pos = yama["url"].split("/")
+              if (pos.length >= 2){
+                let lat = Number.parseFloat(pos[pos.length-2])
+                let lng = Number.parseFloat(pos[pos.length-1])
+                return (
+                  <Marker position={[lat,lng]} key={lat+","+lng}>
+                    <Popup>
+                      {yama["山名<山頂名>"]} <br/>
+                      {lat},{lng} <br/>
+                      標高: {yama["標高"]} <br/>
+                      都道府県: {yama["都道府県"]} <br/>
+                    </Popup>
+                  </Marker>
+                )
+              }else{
+                return(<></>)
+              }
+            }else{
+              return(<></>)
+            }
+          })
+        }      
+      </MapContainer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
